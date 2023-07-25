f43bea806806f33b57cb5d59a60ec3bc570f29ed
ikovacic <kovacicmail75@gmail.com>
2023-07-25 13:37:48 +0200

    Enemy (cons, get, set) + enemy randomizer return

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,10 +1,37 @@
 package src;
-
 public class Enemy {
-  private String name;
-  private int hp;
-  private int def;
-  private int atk;
+  int atk;
 
+  public int getAtk() {
+    return atk;
+  }
 
+  public void setAtk(int atk) {
+    this.atk = atk;
+  }
+
+  public void setHp(int hp) {
+    this.hp = hp;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getHp() {
+    return hp;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Enemy(int atk, int hp, String name) {
+    this.atk = atk;
+    this.hp = hp;
+    this.name = name;
+  }
+
+  int hp;
+  String name;
 }
---
+++ src/EnemyRand.java
@@ -0,0 +1,31 @@
+package src;
+import java.util.Random;
+
+public class EnemyRand {
+    public String rand() {
+        Enemy archer = new Enemy(3, 14, "Archer");
+        Enemy rouge = new Enemy(4, 13, "Rouge");
+        Enemy ninja = new Enemy(5, 15, "Ninja");
+        String encounter = null;
+        Random rand = new Random();
+        int max = 2;
+        int min = 1;
+        int value = rand.nextInt(max + min) + min;
+        switch (value) {
+            case 1:
+                encounter = archer.name;
+                return encounter;
+
+            case 2:
+                encounter = rouge.name;
+                return encounter;
+
+
+            case 3:
+                encounter = ninja.name;
+                return encounter;
+        }
+
+        return encounter;
+    }
+}
