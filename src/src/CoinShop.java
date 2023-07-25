43d72ba95d6ffc40c2448e8f01369d4be2629d1a
Martin <martin.bosa324@gmail.com>
2023-07-25 11:14:29 +0200

    Files

--- src/src/CoinShop.java
+++ src/src/CoinShop.java
@@ -34,6 +34,45 @@ public class CoinShop{
    public int getPrice() {
       return price;
    }
+   /*
+        CoinShop coinShop = new CoinShop();
+        int wallet = 50;
+        int enemy = 5;
+        int swordDamage = 10;
+        coinShop.addItem("healthPotions");
+
+        Inventory playerInventory = new Inventory();
+        playerInventory.addItem("Sword");
+        playerInventory.addItem("shield");
+
+
+        Scanner something = new Scanner(System.in);
+        if(enemy < swordDamage){
+            System.out.println("you have defeated an enemy");
+            wallet = wallet + 2;
+        }
+        else{
+            System.out.println("enemy has slain you");
+        }
+
+        System.out.println("press P to open your inventory or move forward");
+
+        while(true){
+            String input = something.nextLine();
+            if(input.equalsIgnoreCase("p")){
+                System.out.println("You have " + wallet + "coins");
+                coinShop.displayShop();
+                System.out.println("press b to buy it");
+                }else if (input.equalsIgnoreCase("b")) {
+                    playerInventory.addItem("health potion");
+                System.out.println("open your inventory ");
+            } else if (input.equalsIgnoreCase("e")) {
+                    playerInventory.displayInventory();
+                }
+            else if (input.equalsIgnoreCase("w")) {
+                playerInventory.moveForward();
+            }
+            } */
 }
 
 
